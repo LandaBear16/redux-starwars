4450172453db54291b0ab889ca56f0b6c0c0651f
Yolanda Collins <landa.collins14@gmail.com>
2023-07-17 21:39:14 +0200

    Adding in a simple api call

--- src/store/charactersSlice.js
+++ src/store/charactersSlice.js
@@ -1,9 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   data: [],
   loading: true,
 };
+
+const ENDPOINT = 'https://star-wars-characters.glitch.me/api/search/';
+
+export const fetchCharactersFromAPI = createAsyncThunk(
+  'characters/search',
+  async (searchTerm) => {
+    const response = await fetch(ENDPOINT + searchTerm).then((response) =>
+      response.json(),
+    );
+    console.log(response);
+    return response.results;
+  },
+);
 
 export const charactersSlice = createSlice({
   name: 'characters',
@@ -11,6 +24,11 @@ export const charactersSlice = createSlice({
   reducers: {
     add: (state, action) => {
       state.characters = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchCharactersFromAPI.fulfilled]: (state, action) => {
+      state.data = action.payload;
     },
   },
 });
